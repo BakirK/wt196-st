532f7b95a823ec0a9cbb2fbe5b41b71344e3d104
Bakir Karovic <bakir.karovic@gmail.com>
2019-11-19 20:19:00 +0100

    Prva verzija

--- kalendar.js
+++ kalendar.js
@@ -1,7 +1,9 @@
 var today = new Date();
 var currentMonth = today.getMonth();
 var months = ["Januar", "Februar", "Mart", "April", "Maj", "Juni", "Juli", "August", "Septembar", "Oktobar", "Novembar", "Decembar"];
-
+var trenutnaSala;
+var sala;
+//sala.options[e.selectedIndex]
 function next() {
     //currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
     currentMonth = (currentMonth + 1) % 12;
@@ -25,73 +27,144 @@ function previous() {
     Kalendar.iscrtajKalendar(document.querySelector(".dateGrid"), currentMonth);
 }
 
+function Semestar(month) {
+    if ((month >= 9 && month <= 11) || month == 0) return "zimski";
+    if(month >= 1 && month <= 5) return "ljetni";
+    return "";
+}
 
+function poklapajuSeDatumi(date1Pocetak, date1Kraj, date2Pocetak, date2Kraj) {
+    if(date1Pocetak > date2Pocetak && date1Pocetak < date2Kraj) return true;
+    if(date1Kraj > date2Pocetak && date1Kraj < date2Kraj) return true;
+    if(date2Pocetak > date1Pocetak && date2Pocetak < date1Kraj) return true;
+    if(date2Kraj > date1Pocetak && date2Kraj < date1Kraj) return true;
+    return false;
+}
 
 var Kalendar = (function(){
-//
-//ovdje idu privatni atributi
-//
-/*function obojiZauzecaImpl(kalendarRef, mjesec, sala, pocetak, kraj){
-//implementacija ide ovdje
-}
-function ucitajPodatkeImpl(periodicna, vanredna){
-//implementacija ide ovdje
-}*/
-function iscrtajKalendarImpl(kalendarRef, month){
-    currentMonth = month;
-    let timeWrapper = document.createElement("time");
-    timeWrapper.innerHTML = months[currentMonth];
-    document.querySelector(".monthIndicator").innerHTML = "";
-    document.querySelector(".monthIndicator").appendChild(timeWrapper);
-	kalendarRef.innerHTML = "";
+    var periodicnaZauzeca, vanrednaZauzeca;
+    let ucitani = false;
+    function obojiZauzecaImpl(kalendarRef, mjesec, sala, pocetak, kraj){
+        if (ucitani) {
+            for (var i = 0; i < periodicnaZauzeca.length; i++) {
+                if(trenutnaSala  == periodicnaZauzeca[i].naziv) {
+                    if(Semestar(mjesec) === periodicnaZauzeca[i].semestar) {
+                        let trenutniDan = document.getElementsByClassName("fake").length;
+                        let pozadine = document.getElementsByClassName("pozadina");
 
-    let firstDay = ((new Date(2019, month)).getDay() + 6) % 7; //shiftaj dane za 6 jer je nedjelja prvi dan
-    let daysInMonth = 32 - new Date(2019, month, 32).getDate();
-    let date = 1;
-    for (let i = 0; i < 6; i++) {
+                        let pocetniDatum = Date.parse('01/01/2011 ' + pocetak + ':00');
+                        let krajniDatum = Date.parse('01/01/2011 ' + kraj + ':00');
+                        let pocetniZauzetiDatum = Date.parse('01/01/2011 ' + periodicnaZauzeca[i].pocetak + ':00');
+                        let krajniZauzetiDatum = Date.parse('01/01/2011 ' + periodicnaZauzeca[i].kraj + ':00');
 
-        //creating individual cells, filing them up with data.
-        for (let j = 0; j < 7; j++) {
-            if (i === 0 && j < firstDay) {
-            	let tempDiv = document.createElement("div");
-            	tempDiv.classList.add("fake");
-            	kalendarRef.appendChild(tempDiv);
-            }
-            else if (date > daysInMonth) {
-                break;
+                        for (var i = 0; i < pozadine.length; i++) {
+                            if(trenutniDan == periodicnaZauzeca[i].dan || 
+                                poklapajuSeDatumi(pocetniDatum, krajniDatum, pocetniZauzetiDatum, krajniZauzetiDatum)) {
+                                pozadine[i].classList.remove("slobodna");
+                                pozadine[i].classList.add("zauzeta");
+                            }
+                            trenutniDan = (trenutniDan + 1)% 7;
+                        }
+                    }
+                }
             }
 
-            else {
-            	let dayWrapper = document.createElement("div");
-            	dayWrapper.classList.add("dayWrapper");
+            for (var i = 0; i < vanrednaZauzeca.length; i++) {
+                if(trenutnaSala == vanrednaZauzeca[i].naziv) {
+                    let datumVanredna = vanrednaZauzeca[i].datum.split(".");
+                    if(mjesec == datum[1]) {
+                        let trenutniDan = document.getElementsByClassName("fake").length;
+                        let pozadine = document.getElementsByClassName("pozadina");
 
-					let buttonDiv = document.createElement("div");
-					buttonDiv.classList.add("buttonDiv");
-						let dugmeTemp = document.createElement("button");
-							let timeTemp = document.createElement("time");
-							timeTemp.innerHTML = date;
-                        dugmeTemp.appendChild(timeTemp);
-                    buttonDiv.appendChild(dugmeTemp);
+                        let pocetniDatum = Date.parse('01/01/2011 ' + pocetak + ':00');
+                        let krajniDatum = Date.parse('01/01/2011 ' + kraj + ':00');
+                        let pocetniZauzetiDatum = Date.parse('01/01/2011 ' + vanrednaZauzeca[i].pocetak + ':00');
+                        let krajniZauzetiDatum = Date.parse('01/01/2011 ' + vanrednaZauzeca[i].kraj + ':00');
 
-					let statusDiv = document.createElement("div");            	
-					statusDiv.classList.add((j%3) == 0 ? "slobodna" : "zauzeta");
-					statusDiv.innerHTML= "&nbsp;"
-                dayWrapper.appendChild(buttonDiv);
-				dayWrapper.appendChild(statusDiv);
-                kalendarRef.appendChild(dayWrapper);
-                date++;
+                        for (var i = 0; i < pozadine.length; i++) {
+                            if(trenutniDan == datumVanredna[0] || 
+                                poklapajuSeDatumi(pocetniDatum, krajniDatum, pocetniZauzetiDatum, krajniZauzetiDatum)) {
+                                pozadine[i].classList.remove("slobodna");
+                                pozadine[i].classList.add("zauzeta");
+                            }
+                            trenutniDan = (trenutniDan + 1)% 7;
+                        }
+                    }
+                }
             }
         }
     }
-}
-return {
-    //obojiZauzeca: obojiZauzecaImpl,
-    //ucitajPodatke: ucitajPodatkeImpl,
-    iscrtajKalendar : iscrtajKalendarImpl
-}
+    function ucitajPodatkeImpl(periodicna, vanredna){
+        periodicnaZauzeca = periodicna;
+        vanrednaZauzeca = vanredna;
+        ucitani = true;
+    }
+    function iscrtajKalendarImpl(kalendarRef, month){
+        currentMonth = month;
+        let timeWrapper = document.createElement("time");
+        timeWrapper.innerHTML = months[currentMonth];
+        document.querySelector(".monthIndicator").innerHTML = "";
+        document.querySelector(".monthIndicator").appendChild(timeWrapper);
+    	kalendarRef.innerHTML = "";
+
+        let firstDay = ((new Date(2019, month)).getDay() + 6) % 7; //shiftaj dane za 6 jer je nedjelja prvi dan
+        let daysInMonth = 32 - new Date(2019, month, 32).getDate();
+        let date = 1;
+        for (let i = 0; i < 6; i++) {
+
+            //creating individual cells, filing them up with data.
+            for (let j = 0; j < 7; j++) {
+                if (i === 0 && j < firstDay) {
+                	let tempDiv = document.createElement("div");
+                	tempDiv.classList.add("fake");
+                	kalendarRef.appendChild(tempDiv);
+                }
+                else if (date > daysInMonth) {
+                    break;
+                }
+
+                else {
+                	let dayWrapper = document.createElement("div");
+                	dayWrapper.classList.add("dayWrapper");
+
+    					let buttonDiv = document.createElement("div");
+    					buttonDiv.classList.add("buttonDiv");
+    						let dugmeTemp = document.createElement("button");
+    							let timeTemp = document.createElement("time");
+    							timeTemp.innerHTML = date;
+                            dugmeTemp.appendChild(timeTemp);
+                        buttonDiv.appendChild(dugmeTemp);
+
+    					let statusDiv = document.createElement("div");            	
+    					statusDiv.classList.add("slobodna");
+                        statusDiv.classList.add("pozadina");
+    					statusDiv.innerHTML= "&nbsp;"
+                    dayWrapper.appendChild(buttonDiv);
+    				dayWrapper.appendChild(statusDiv);
+                    kalendarRef.appendChild(dayWrapper);
+                    date++;
+                }
+            }
+        }
+    }
+    return {
+        //obojiZauzeca: obojiZauzecaImpl,
+        ucitajPodatke: ucitajPodatkeImpl,
+        iscrtajKalendar : iscrtajKalendarImpl
+    }
 }());
 
 document.addEventListener('DOMContentLoaded', function () {
+    //listener za promjenu odabira u listi sala
+    sala = document.getElementById("sale");
+    if(sala != null) {
+        trenutnaSala = sala.options[sala.selectedIndex].innerHTML;
+        sala.addEventListener('change', function() {
+            trenutnaSala = sala.options[sala.selectedIndex].innerHTML;
+            console.log(trenutnaSala);
+        }, false);
+    }
+   
     Kalendar.iscrtajKalendar(document.querySelector(".dateGrid"), currentMonth);
     document.querySelector(".prev").addEventListener( "click", function( ev ) {
         previous();
@@ -101,4 +174,5 @@ document.addEventListener('DOMContentLoaded', function () {
     document.querySelector(".next").addEventListener( "click", function( ev ) {
         next();
     }, false);
+    
 });
